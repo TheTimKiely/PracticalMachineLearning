a0f6e1879a61317b8e65bbc6a0674baa460360ed
Tim Kiely <thetimkiely@gmail.com>
2018-01-18 17:05:40 -0500

    Added Convnet example

---
+++ Python/DL/Convnet.py
@@ -0,0 +1,57 @@
+from keras import models
+from keras import layers
+import time
+import matplotlib.pyplot as plt
+
+from PracticalMLCore.PracticalMLUtils import plot_metrics
+
+start = time.time()
+model = models.Sequential()
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)))
+model.add(layers.MaxPooling2D((2,2)))
+model.add(layers.Conv2D(64, (3,3), activation='relu'))
+model.add(layers.MaxPooling2D(2,2))
+model.add(layers.Conv2D(64, (3,3), activation='relu'))
+
+model.add(layers.Flatten())
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(10, activation='softmax'))
+
+print(model.summary())
+
+from keras.datasets import mnist
+from keras.utils import to_categorical
+
+(raw_train_images, raw_train_labels), (raw_test_images, raw_test_labels) = mnist.load_data()
+
+train_samples = 60000
+train_images = raw_train_images[:60000]
+train_labels = raw_train_labels[:60000]
+train_images = train_images.reshape(60000, 28, 28, 1)
+train_images = train_images.astype('float32') / 255
+
+test_samples = 10000
+test_images = raw_test_images[:test_samples]
+test_labels = raw_test_labels[:test_samples]
+test_images = test_images.reshape(test_samples, 28, 28, 1)
+test_images = test_images.astype('float32') / 255
+
+train_labels = to_categorical(train_labels)
+test_labels = to_categorical(test_labels)
+
+model.compile(optimizer='rmsprop',
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+
+history = model.fit(train_images, train_labels, epochs=5, batch_size=64)
+
+#plot_metrics(history)
+
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+print(f'Accuracy: {test_acc} Loss: {test_loss}')
+
+#plt.imshow(raw_test_images[0], cmap=plt.cm.gray_r, interpolation='nearest')
+#prediction = model.predict(test_images)
+#print(f'Prediction: {prediction}')
+
+print(f'Time: {time.time() - start}')
