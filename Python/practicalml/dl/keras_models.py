9a974514914494dc925d164835af037b750d65aa
Tim Kiely <thetimkiely@gmail.com>
2018-01-22 13:40:57 -0500

    Added weights file

--- Python/practicalml/dl/keras_models.py
+++ Python/practicalml/dl/keras_models.py
@@ -1,7 +1,9 @@
+import os
 from keras.applications import VGG16
 
 class ModelRepository(object):
     def get_vgg16(self):
-        path = 'D:\code\ML\models\\vgg16_weights_tf_dim_ordering_tf_kernels_notop.h5'
-        vgg16 = VGG16(weights='imagenet', weights_path=path, include_top=False, input_shape=(150, 150, 3))
+        #path = 'D:\code\ML\models\\vgg16_weights_tf_dim_ordering_tf_kernels_notop.h5'
+        path = os.path.join(os.getcwd(), 'vgg16_weights_tf_dim_ordering_tf_kernels_notop.h5')
+        vgg16 = VGG16(weights=path, include_top=False, input_shape=(150, 150, 3))
         return vgg16
